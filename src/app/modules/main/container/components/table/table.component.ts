e69be4598d3f57b3fb5d24329b2568099a3e0e64
Martin <m.quintana@buenosaires.gob.ar>
2020-02-13 16:50:21 -0300

    table ready

--- src/app/modules/main/container/components/table/table.component.ts
+++ src/app/modules/main/container/components/table/table.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  selector: "app-table",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.css"]
 })
 export class TableComponent implements OnInit {
+  elementsList = [
+    {
+      id: 1,
+      name: "Hydrogen",
+      type: "Metal",
+      value: "H"
+    },
+    {
+      id: 2,
+      name: "Helium",
+      type: "Gas",
+      value: "He"
+    },
+    {
+      id: 3,
+      name: "Lithium",
+      type: "Metal",
+      value: "Li"
+    },
+    {
+      id: 4,
+      name: "Beryllium",
+      type: "Metal",
+      value: "H"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
